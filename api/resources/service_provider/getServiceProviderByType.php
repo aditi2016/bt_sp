aacc30b330c2e060109ce724cf729a5a07da17a3
aditi2016 <aditilahoria@gmail.com>
2016-11-12 17:01:20 +0530

    first commit

--- api/resources/service_provider/getServiceProviderByType.php
+++ api/resources/service_provider/getServiceProviderByType.php
@@ -9,8 +9,8 @@ function getServiceProviderByType(){
     global $app;
     $type = $app->request()->get('type');
     
-    $gender_str = ($gender!=null)?" AND gender = :gender ":"";
     $sql = "SELECT * FROM `services` as s inner join service_provider_service_mapping as m inner join service_providers as sp WHERE s.`name`  = :type and s.`status` = 'active' and s.id = m.service_id and m.`service_provider_id` = sp.id";
+    
     try {
         $db = getDB();
         $stmt = $db->prepare($sql);
