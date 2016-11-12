0b4a81c7d5707e4bbf0b382e197a424909d6ec3d
aditi2016 <aditilahoria@gmail.com>
2016-11-12 17:02:22 +0530

    first commit

--- api/db.php
+++ api/db.php
@@ -2,7 +2,7 @@
 function getDB() {
 	$dbhost="localhost";
 	$dbuser="root";
-	$dbpass="redhat11111p";
+	$dbpass="redhat@11111p";
 	$dbname="blueteam_service_providers";
 	$dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
 	$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
