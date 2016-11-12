b04106ab8def77365353e0f484898ea7aacdfb55
aditi2016 <aditilahoria@gmail.com>
2016-11-12 16:42:24 +0530

    first commit

---
+++ api/app.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spider-ninja
+ * Date: 6/4/16
+ * Time: 1:12 PM
+ */
+
+\Slim\Slim::registerAutoloader();
+
+global $app;
+
+if(!isset($app))
+    $app = new \Slim\Slim();
+
+
+//$app->response->headers->set('Access-Control-Allow-Origin',  'http://localhost');
+$app->response->headers->set('Access-Control-Allow-Credentials',  'true');
+/*$app->response->headers->set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+$app->response->headers->set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+*/
+$app->response->headers->set('Content-Type', 'application/json');
+
+/* Starting routes */
+
+$app->get('/service_provider','getServiceProviderByType');
+$app->get('/service_provider/:id','getServiceProviderById');
+
+
+$app->post('/service_provider', 'insertServiceProvider');
+$app->put('/service_provider/:id','updateServiceProvider');
+$app->post('/service_provider/:id','updateServiceProvider');
+
+$app->post('/auth', 'userAuth');
+
+/* Ending Routes */
+
+$app->run();
---
+++ api/resources/service_provider/getServiceProviderByType.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spider-ninja
+ * Date: 6/4/16
+ * Time: 1:32 PM
+ */
+function getServiceProviderByType(){
+    global $app;
+    $type = $app->request()->get('type');
+    
+    $gender_str = ($gender!=null)?" AND gender = :gender ":"";
+    $sql = "SELECT * FROM `services` as s inner join service_provider_service_mapping as m inner join service_providers as sp WHERE s.`name`  = :type and s.`status` = 'active' and s.id = m.service_id and m.`service_provider_id` = sp.id";
+    try {
+        $db = getDB();
+        $stmt = $db->prepare($sql);
+        
+        $stmt->bindParam("type", $type);
+        
+        $stmt->execute();
+        $candidates = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo '{"candidates": ' . json_encode($candidates) . '}';
+    } catch (PDOException $e) {
+        //error_log($e->getMessage(), 3, '/var/tmp/php.log');
+        echo '{"error":{"text":' . $e->getMessage() . '}}';
+    }
+}
