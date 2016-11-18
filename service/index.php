876cc710a30f8a4a6e77d2445e48a435c4f8a9c7
anil-ninja <kumar.anil8892@gmail.com>
2016-11-18 17:05:36 +0530

    service provider added

--- service/index.php
+++ service/index.php
@@ -24,6 +24,11 @@ $allServices = mysqli_query($dbHandle, "SELECT a.price, a.nagotiable, b.name, b.
                                             WHERE a.service_provider_id = '$serviceProviderId'
                                             AND a.service_id = b.id AND b.status = 'active' ;");
 
+$recommendedServices = mysqli_query($dbHandle, "SELECT a.price, a.nagotiable, b.name, b.pic_id, b.description
+                                            FROM service_provider_service_mapping AS a
+                                            JOIN services AS b
+                                            WHERE a.service_id = b.id AND b.status = 'active' 
+                                            ORDER BY RAND() LIMIT 5;");
 
 // vendor other service
 //$otherServices
@@ -59,48 +64,51 @@ $donotLikeCount*/
    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 </head>
 <body>
-    <div id="wrapper">
-        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header" style="float: right;">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" ><?=$serviceProviderData['name']; ?></a> 
-            </div>
-            <div style="color: white;padding: 15px 50px 5px 50px;float: left;font-size: 16px;"> 
-                Last access : 30 May 2014 &nbsp; 
-                <a href="#" class="btn btn-danger square-btn-adjust">Logout</a> 
-            </div>
-        </nav>   
-        <!-- /. NAV TOP  -->
-        <nav class="navbar-default navbar-side pull-right" role="navigation">
-            <div class="sidebar-collapse">
-                <ul class="nav" id="main-menu">
+    
+    <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header" style="float: right;">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" ><?=$serviceProviderData['name']; ?></a> 
+        </div>
+        <div style="color: white;padding: 15px 50px 5px 50px;float: left;font-size: 16px;"> 
+            Last access : 30 May 2014 &nbsp; 
+            <a href="#" class="btn btn-danger square-btn-adjust">Logout</a> 
+        </div>
+    </nav>   
+    <!-- /. NAV TOP  -->
+    <nav class="navbar-default navbar-side pull-right" role="navigation">
+        <div class="sidebar-collapse">
+            <ul class="nav" id="main-menu">
                 <li class="text-center">
                     <img src="<?=$profilePic; ?>" class="user-image img-responsive"/>
-                    </li>
-                    <li>
-                        <a class="active-menu"  ><i class="fa fa-flag fa-2x"></i> 
-                        <?=$serviceProviderData['organization']; ?></a>
-                    </li>
-                     <li>
-                        <a  ><i class="fa fa-bars fa-2x"></i>
-                        <?=$serviceProviderData['description']; ?></a>
-                    </li>
-                    <li>
-                        <a  ><i class="fa fa-home fa-2x"></i>
-                        <?=$serviceProviderData['address']; ?></a>
-                    </li>
-                        
-                </ul>
-               
-            </div>
+                </li>
+                <li>
+                    <a class="active-menu"  ><i class="fa fa-flag fa-2x"></i> 
+                    <?=$serviceProviderData['organization']; ?></a>
+                </li>
+                 <li>
+                    <a  ><i class="fa fa-bars fa-2x"></i>
+                    <?=$serviceProviderData['description']; ?></a>
+                </li>
+                <li>
+                    <a  ><i class="fa fa-home fa-2x"></i>
+                    <?=$serviceProviderData['address']; ?></a>
+                </li>
+                
+            </ul>
             
-        </nav>  
+            
+            
+        </div>
+        
+    </nav>  
         <!-- /. NAV SIDE  -->
+    <div id="wrapper">
         <div id="page-wrapper" >
             <div id="page-inner">
                 <div class="row">
@@ -140,7 +148,8 @@ $donotLikeCount*/
                                 <i class="fa fa-rocket"></i>
                             </span>
                             <div class="text-box" >
-                                <p class="main-text">120</p>
+                                <p class="text-muted">Awesome</p>
+                                <p style="margin-left: 80px" class="main-text">120</p>
                             </div>
                         </div>
                     </div>
@@ -150,7 +159,8 @@ $donotLikeCount*/
                                 <i class="fa fa-heart"></i>
                             </span>
                             <div class="text-box" >
-                                <p class="main-text">30</p>
+                                <p class="text-muted">Good</p>
+                                <p style="margin-left: 80px" class="main-text">30</p>
                             </div>
                         </div>
                     </div>
@@ -160,7 +170,8 @@ $donotLikeCount*/
                                 <i class="fa fa-check"></i>
                             </span>
                             <div class="text-box" >
-                                <p class="main-text">240</p>
+                                <p class="text-muted">Average</p>
+                                <p style="margin-left: 80px" class="main-text">240</p>
                             </div>
                         </div>
                     </div>
@@ -170,32 +181,14 @@ $donotLikeCount*/
                                 <i class="fa fa-thumbs-o-down"></i>
                             </span>
                             <div class="text-box" >
-                                <p class="main-text">3 </p>
+                                <p class="text-muted">Not Good</p>
+                                <p style="margin-left: 80px" class="main-text">3 </p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <!-- /. ROW  -->
-                <hr />
-                <div class="row">
-                <?php
-                    while ($allServicesOfVendor = mysqli_fetch_array($allServices)) {
-                        echo "<div class='col-md-3 col-sm-6 col-xs-12'>
-                                <div class='panel panel-primary text-center no-boder bg-color-green'>
-                                    <div class='panel-body'>
-                                        <img src='http://api.file-dog.shatkonlabs.com/files/rahul/".$allServicesOfVendor['pic_id']."' class='service-image img-responsive'/>
-                                        <span class='service-name'>".$allServicesOfVendor['price']." Rs per Hour <br/>Nagotiable : ".strtoupper($allServicesOfVendor['nagotiable'])." </span>
-                                    </div>
-                                    <div class='panel-footer back-footer-green'>
-                                       ".$allServicesOfVendor['name']." <br/>
-                                       ".$allServicesOfVendor['description']."
-                                    </div>
-                                </div>
-                            </div>";
-                    }
-                ?>
-                    
-                </div>
+
                 <div class="row">
                     <div class="col-md-12 col-sm-12 col-xs-12">
                         <div class="chat-panel panel panel-default chat-boder chat-panel-head" >
@@ -306,7 +299,50 @@ $donotLikeCount*/
                     </div>
                     
                 </div>     
-                <!-- /. ROW  -->           
+                <!-- /. ROW  -->
+                <hr />
+                Other Services 
+                <div class="row">
+                <?php
+                    while ($allServicesOfVendor = mysqli_fetch_array($allServices)) {
+                        echo "<div class='col-md-3 col-sm-6 col-xs-12'>
+                                <div class='panel panel-primary text-center no-boder bg-color-blue'>
+                                    <div class='panel-body'>
+                                        <img src='http://api.file-dog.shatkonlabs.com/files/rahul/".$allServicesOfVendor['pic_id']."' class='service-image img-responsive'/>
+                                        <span class='service-name'>".$allServicesOfVendor['price']." Rs per Hour <br/>Nagotiable : ".strtoupper($allServicesOfVendor['nagotiable'])." </span>
+                                    </div>
+                                    <div class='panel-footer back-footer-blue'>
+                                       ".$allServicesOfVendor['name']." <br/>
+                                       ".$allServicesOfVendor['description']."<br/>
+                                       <a href='bluetem.in/app/' class='btn btn-info'>Book Now</a>
+                                    </div>
+                                </div>
+                            </div>";
+                    }
+                ?>
+                </div>
+                <hr/>
+                Recommended Services
+                <div class="row">
+                
+            <?php
+                while ($allRecommendedServices = mysqli_fetch_array($recommendedServices)) {
+                    echo "<div class='col-md-3 col-sm-6 col-xs-12'>
+                            <div class='panel panel-primary text-center no-boder bg-color-blue'>
+                                <div class='panel-body'>
+                                    <img src='http://api.file-dog.shatkonlabs.com/files/rahul/".$allRecommendedServices['pic_id']."' class='service-image img-responsive'/>
+                                    <span class='service-name'>".$allRecommendedServices['price']." Rs per Hour <br/>Nagotiable : ".strtoupper($allRecommendedServices['nagotiable'])." </span>
+                                </div>
+                                <div class='panel-footer back-footer-blue'>
+                                   ".$allRecommendedServices['name']." <br/>
+                                   ".$allRecommendedServices['description']."<br/>
+                                   <a href='bluetem.in/app/' class='btn btn-info'>Book Now</a>
+                                </div>
+                            </div>
+                        </div>";
+                }
+            ?>      
+                </div>           
             </div>
             <!-- /. PAGE INNER  -->
         </div>
