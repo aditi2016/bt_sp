2d042d7e36fc39a2e0d9b1aba04f4872ad87fc51
rahullahoria <rahul_lahoria@yahoo.com>
2016-11-23 18:33:21 +0530

    search, feedback request, invoice apis added

--- api/resources/service_provider/getServiceProvider.php
+++ api/resources/service_provider/getServiceProvider.php
@@ -8,7 +8,7 @@
 
 function getServiceProvider($id){
 
-    $sql = "SELECT a.name, a.organization, a.description, a.experience, a. a.id, a.profile_pic_id, a.`reliability_score`, a.`reliability_count`,
+    $sql = "SELECT a.name, a.organization, a.description, a.experience, a.id, a.profile_pic_id, a.`reliability_score`, a.`reliability_count`,
                 b.price,
             b.nagotiable, b.hourly, b.status FROM service_providers AS a JOIN service_provider_service_mapping
             AS b WHERE a.id = b.service_provider_id AND a.id = :id ";
