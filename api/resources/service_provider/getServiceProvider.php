1fd436434c88f8c2b77d6dccbb845c5fb591878f
rahullahoria <rahul_lahoria@yahoo.com>
2016-11-23 18:30:41 +0530

    search, feedback request, invoice apis added

---
+++ api/resources/service_provider/getServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spider-ninja
+ * Date: 11/23/16
+ * Time: 6:22 PM
+ */
+
+function getServiceProvider($id){
+
+    $sql = "SELECT a.name, a.organization, a.description, a.experience, a. a.id, a.profile_pic_id, a.`reliability_score`, a.`reliability_count`,
+                b.price,
+            b.nagotiable, b.hourly, b.status FROM service_providers AS a JOIN service_provider_service_mapping
+            AS b WHERE a.id = b.service_provider_id AND a.id = :id ";
+
+
+
+    try {
+        $db = getDB();
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam("id", $id);
+
+        $stmt->execute();
+        $serviceProviders = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+
+        $db = null;
+        echo '{"service_provider": ' . json_encode($serviceProviders) . '}';
+
+    } catch (PDOException $e) {
+        //error_log($e->getMessage(), 3, '/var/tmp/php.log');
+        echo '{"error":{"text":' . $e->getMessage() . '}}';
+    }
+
+}
