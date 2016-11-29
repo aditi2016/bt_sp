5f2c97a3d612f8986dc78f8ab9b0c279af124df7
rahullahoria <rahul_lahoria@yahoo.com>
2016-11-29 19:39:24 +0530

    search, feedback request, invoice apis added

--- api/resources/location/getLocationDetails.php
+++ api/resources/location/getLocationDetails.php
@@ -41,8 +41,8 @@ function getLocationDetails($id){
 function getGPSLocationDetails($loc){
     $url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=$loc&sensor=true";
 
-    var_dump(httpGet($url));die();
     $details = json_decode(httpGet($url));
+    var_dump($details);die();
 
 
     $return = array();
