a538c8d210e2b6040db740540956a0a6d28b9eef
rahullahoria <rahul_lahoria@yahoo.com>
2016-12-03 17:42:30 +0530

    search, feedback request, invoice apis added

--- api/resources/location/getLocationDetails.php
+++ api/resources/location/getLocationDetails.php
@@ -77,20 +77,20 @@ function getGPSLocationDetails($loc){
                         $return['city'] = array('name' => $acValue->long_name);
                     } elseif (!$flag &&  !is_bool(array_search('sublocality_level_1', $acValue->types) )) {
                         if(isset($return['area']['name']))
-                            $return['area']['name'] = $acValue->long_name . "-" . $return['area']['name'] ;
+                            $return['area']['name'] = $return['area']['name'] . "-" . $acValue->long_name;
                         else
                             $return['area'] = array('name' => $acValue->long_name);
                         $flag = true;
 
                     } elseif ($area_accuracy <= 1 && !is_bool(array_search('sublocality_level_2', $acValue->types)) ) {
                         if($flag)
-                            $return['area']['name'] = $return['area']['name'] . "-" . $acValue->long_name;
+                            $return['area']['name'] = $acValue->long_name . "-" . $return['area']['name'] ;
                         else
                             $return['area'] = array('name' => $acValue->long_name);
                         $area_accuracy = 2;
                     } elseif ($area_accuracy <= 2 && !is_bool(array_search('sublocality_level_3', $acValue->types) )) {
                         if($flag)
-                            $return['area']['name'] = $return['area']['name'] . "-" . $acValue->long_name;
+                            $return['area']['name'] = $acValue->long_name . "-" . $return['area']['name'] ;
                         else
                             $return['area'] = array('name' => $acValue->long_name);
                         $area_accuracy = 3;
