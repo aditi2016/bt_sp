9cf05187af196f3fd3ca8f6bd40ad023b1f74f62
rahullahoria <rahul_lahoria@yahoo.com>
2016-12-08 17:18:53 +0530

    search, feedback request, invoice apis added

---
+++ api/resources/services/servicesNeedLookUp.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spider-ninja
+ * Date: 12/8/16
+ * Time: 5:11 PM
+ */
+
+/*
+ * SELECT a.id, a.service_id, b.name AS service_name, a.area_id, c.name AS area_name, c.city_id, d.name AS city_name, a.result_count, a.count
+FROM `service_looks` AS a
+INNER JOIN services AS b
+INNER JOIN areas AS c
+INNER JOIN cities AS d
+WHERE a.result_count <6
+AND a.area_id = c.id
+AND a.service_id = b.id
+AND c.city_id = d.id
+ * */
+
+function servicesNeedLookUp(){
+
+    $sql = "SELECT
+              a.id, a.service_id, b.name AS service_name, a.area_id, c.name AS area_name, c.city_id,
+              d.name AS city_name, a.result_count, a.count
+              FROM `service_looks` AS a
+                INNER JOIN services AS b
+                INNER JOIN areas AS c
+                INNER JOIN cities AS d
+                  WHERE a.result_count <6
+                        AND a.area_id = c.id
+                        AND a.service_id = b.id
+                        AND c.city_id = d.id ";
+
+    try {
+        $db = getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $services = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo '{"services": ' . json_encode($services) . '}';
+    } catch (PDOException $e) {
+        //error_log($e->getMessage(), 3, '/var/tmp/php.log');
+        echo '{"error":{"text":' . $e->getMessage() . '}}';
+    }
+
+}
