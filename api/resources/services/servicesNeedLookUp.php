3746b4173d22d0d14a3183d5018fa3742a36bb1a
rahullahoria <rahul_lahoria@yahoo.com>
2016-12-08 17:45:08 +0530

    search, feedback request, invoice apis added

--- api/resources/services/servicesNeedLookUp.php
+++ api/resources/services/servicesNeedLookUp.php
@@ -21,7 +21,8 @@ AND c.city_id = d.id
 function servicesNeedLookUp(){
 
     $sql = "SELECT
-              a.id, a.service_id, b.name AS service_name, a.area_id, c.name AS area_name, c.gps_location, c.city_id,
+              a.id, a.service_id, b.name AS service_name, a.area_id, c.name AS area_name,
+              X(c.gps_location) as lat_gps_location , Y(c.gps_location) as lng_gps_location , c.city_id,
               d.name AS city_name, a.result_count, a.count
               FROM `service_looks` AS a
                 INNER JOIN services AS b
@@ -39,8 +40,8 @@ function servicesNeedLookUp(){
         $services = $stmt->fetchAll(PDO::FETCH_OBJ);
 
         foreach($services as $service){
-            $url = "http://api.sp.blueteam.in/service/".$service->service_id."?location=".$service->gps_location;
-            json_decode(httpGet($url));
+            $url = "http://api.sp.blueteam.in/service/".$service->service_id."?location=".$service->lat_gps_location.",".$service->lng_gps_location;
+            httpGet($url);
         }
 
         $stmt = $db->prepare($sql);
