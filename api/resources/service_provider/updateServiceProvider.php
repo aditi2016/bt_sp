98cae283ddaddf1616f34f048bde4e51661ee1d9
rahullahoria <rahul_lahoria@yahoo.com>
2016-12-16 12:54:32 +0530

    search, feedback request, invoice apis added

--- api/resources/service_provider/updateServiceProvider.php
+++ api/resources/service_provider/updateServiceProvider.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Created by PhpStorm.
  * User: spider-ninja
@@ -8,7 +8,7 @@
 
 
 
-   
+
 function updateServiceProvider($id){
 
     $request = \Slim\Slim::getInstance()->request();
@@ -16,45 +16,82 @@ function updateServiceProvider($id){
 
     $serviceProvider = json_decode($request->getBody());
     if (is_null($serviceProvider)){
-      echo '{"error":{"text":"Invalid Json"}}';
-      die();
+        echo '{"error":{"text":"Invalid Json"}}';
+        die();
     }
-                $sql = "UPDATE 
+
+    $updateStr = "";
+
+    if(isset($serviceProvider->name))
+        $updateStr .= "name =:name,";
+
+    if(isset($serviceProvider->organization))
+        $updateStr .= "organization =:organization,";
+
+    if(isset($serviceProvider->description))
+        $updateStr .= "description =:description,";
+
+    if(isset($serviceProvider->description))
+        $updateStr .= "description =:description,";
+
+    if(isset($serviceProvider->area_id))
+        $updateStr .= "area_id =:area_id,";
+
+    if(isset($serviceProvider->area_id))
+        $updateStr .= "city_id =:city_id,";
+
+    if(isset($serviceProvider->area_id))
+        $updateStr .= "email =:email,";
+
+    if(isset($serviceProvider->area_id))
+        $updateStr .= "profile_pic_id =:profile_pic_id,";
+
+
+    if(isset($serviceProvider->gps_location)) {
+        $p = explode(",",$serviceProvider->gps_location);
+        $point = $p[0]." ".$p[1];
+        $updateStr .= "gps_location =:GeomFromText( 'POINT(" . $point . ")' ),";
+
+    }
+
+    $updateStr = rtrim($updateStr, ",");
+
+    $sql = "UPDATE
                  service_providers 
                   SET
-                   name =:name, 
-                   organization= :organization,
-                    description=:description,
-                     mobile_no=:mobile,
-                      area_id=:area_id,
-                       city_id=:city_id,
-                        address=:address,
-                         email=:email,
-                         profile_pic_id= :profile_pic_id
+                   $updateStr
                          WHERE id=:service_providers_id";
 
 
 
-        try {
+    try {
         $db = getDB();
         $stmt = $db->prepare($sql);
         //$service_provider->status = "new";
 
-        $stmt->bindParam("name", $serviceProvider->name);
-        $stmt->bindParam("organization", $serviceProvider->organization);
-        $stmt->bindParam("description", $serviceProvider->description);
-        $stmt->bindParam("mobile", $serviceProvider->mobile);
-        $stmt->bindParam("area_id", $serviceProvider->area_id);
-        $stmt->bindParam("city_id", $serviceProvider->city_id);
-        $stmt->bindParam("address", $serviceProvider->address);
-        $stmt->bindParam("email", $serviceProvider->email);
-         $stmt->bindParam("profile_pic_id", $serviceProvider->profile_pic_id);
-         $stmt->bindParam("service_providers_id", $id);
-       
+        if(isset($serviceProvider->name))
+            $stmt->bindParam("name", $serviceProvider->name);
+        if(isset($serviceProvider->organization))
+            $stmt->bindParam("organization", $serviceProvider->organization);
+        if(isset($serviceProvider->description))
+            $stmt->bindParam("description", $serviceProvider->description);
+        if(isset($serviceProvider->area_id))
+            $stmt->bindParam("area_id", $serviceProvider->area_id);
+        if(isset($serviceProvider->city_id))
+            $stmt->bindParam("city_id", $serviceProvider->city_id);
+        if(isset($serviceProvider->address))
+            $stmt->bindParam("address", $serviceProvider->address);
+        if(isset($serviceProvider->email))
+            $stmt->bindParam("email", $serviceProvider->email);
+        if(isset($serviceProvider->profile_pic_id))
+            $stmt->bindParam("profile_pic_id", $serviceProvider->profile_pic_id);
+
+        $stmt->bindParam("service_providers_id", $id);
+
 
         $stmt->execute();
 
-       
+
         $db = null;
         echo '{"service_providers": ' . json_encode($serviceProvider) . '}';
     } catch (PDOException $e) {
