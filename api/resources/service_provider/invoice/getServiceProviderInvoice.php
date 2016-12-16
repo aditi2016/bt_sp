4023c0ab1f2c745362951d4f100f2f7eb90ff9e2
rahullahoria <rahul_lahoria@yahoo.com>
2016-12-16 16:56:43 +0530

    search, feedback request, invoice apis added

--- api/resources/service_provider/invoice/getServiceProviderInvoice.php
+++ api/resources/service_provider/invoice/getServiceProviderInvoice.php
@@ -8,9 +8,15 @@
 
 function getServiceProviderInvoice($id){
 
-    $sql = "SELECT * FROM `invoice` WHERE service_provider_id = :id";
+    $sql = "SELECT * FROM `invoice` WHERE service_provider_id = :id AND Month( creation ) = Month( CURDATE( ) ) x";
 
     //$photosSql = "SELECT photo_id FROM `photos` WHERE `service_provider_id` = :id";
+
+    $sqlMonthYear = "SELECT DISTINCT Month( `creation` ) AS
+                        MONTH , Year( `creation` ) AS year, sum( amount ) as amount
+                        FROM `invoice`
+                        WHERE `service_provider_id` = :id
+                        GROUP BY MONTH , year";
 
     try {
         $db = getDB();
@@ -20,6 +26,13 @@ function getServiceProviderInvoice($id){
 
         $stmt->execute();
         $invoices = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+        $stmt = $db->prepare($sqlMonthYear);
+
+        $stmt->bindParam("id", $id);
+
+        $stmt->execute();
+        $invoices->months = $stmt->fetchAll(PDO::FETCH_OBJ);
 
 
 
