012e5652e9d75e0b48d04325b710b04df4d6ba5c
rahullahoria <rahul_lahoria@yahoo.com>
2016-12-20 17:39:11 +0530

    search, feedback request, invoice apis added

---
+++ sandbox/facebookShareApp/app/src/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['callback_url']         =   'http://blueteam.in/sandbox/facebook/app/?fbTrue=true'; //   /?fbTrue=true allow you to code process section.
+
+//Facebook configuration
+$config['App_ID']      =   '220179075101428';
+$config['App_Secret']  =   '911c170641c8b38c8407f199f0b47c9d';
+
+?>
