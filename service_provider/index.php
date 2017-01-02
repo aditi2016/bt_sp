5397f22d454bac07a6394a5ee17122a002a0af58
anil-ninja <kumar.anil8892@gmail.com>
2017-01-02 20:32:37 +0530

    meta tags included

--- service_provider/index.php
+++ service_provider/index.php
@@ -700,6 +700,16 @@ $metaDescription = implode(',', array_keys(extractCommonWords($metaData)));
         todayBtn: true});
 	  } );
 	</script>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-70488081-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 	<script src="index_files/business_ltd_1.0.js"></script>
 </body>
 </html>
