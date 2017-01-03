fcd44695aca4422c856fd8b55a8e6efa460035ce
anil-ninja <kumar.anil8892@gmail.com>
2017-01-03 16:43:58 +0530

    templates for images

--- sandbox/html2image.php
+++ sandbox/html2image.php
@@ -12,7 +12,7 @@ $link = $_GET['link'];
 
 //var_dump(file_get_contents(call_p2i($bgImg,$logo,$focus,$target."<br/><br/>".$link)));die();
 unlink("Tmpfile.png");
-file_put_contents("Tmpfile.png", fopen(call_p2i($bgImg,$logo,$focus,$target."<br/><br/>".$link),'r'));
+file_put_contents("Tmpfile.png", fopen(call_p2i($bgImg,$logo,$focus,$target."\n\n".$link),'r'));
 
 echo upload_file("Tmpfile.png");
 
