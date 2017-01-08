246cafefe4a041df384816b236b7c21ea86d1d67
rahullahoria <rahul_lahoria@yahoo.com>
2017-01-08 19:20:36 +0530

    search, feedback request, invoice apis added

--- scripts/fbPoster.php
+++ scripts/fbPoster.php
@@ -6,6 +6,40 @@
  * Time: 10:18 PM
  */
 
+function extractCommonWords($string){
+    $stopWords = array('i','a','about','an','and','are','as','at','be','by','com','de','en','for','from',
+        'how','in','is','it','la','of','on','or','that','the','this','to','was','what','when','where','who','will',
+        'with','und','the','www','you','your','yours','true');
+
+    $string = preg_replace('/\s\s+/i', '', $string); // replace whitespace
+    $string = trim($string); // trim the string
+    $string = preg_replace('/[^a-zA-Z0-9 -]/', '', $string); // only take alphanumerical characters, but keep the spaces and dashes too…
+    $string = strtolower($string); // make it lowercase
+
+    preg_match_all('/\b.*?\b/i', $string, $matchWords);
+    $matchWords = $matchWords[0];
+
+    foreach ( $matchWords as $key=>$item ) {
+        if ( $item == '' || in_array(strtolower($item), $stopWords) || strlen($item) <= 3 ) {
+            unset($matchWords[$key]);
+        }
+    }
+    $wordCountArr = array();
+    if ( is_array($matchWords) ) {
+        foreach ( $matchWords as $key => $val ) {
+            $val = strtolower($val);
+            if ( isset($wordCountArr[$val]) ) {
+                $wordCountArr[$val]++;
+            } else {
+                $wordCountArr[$val] = 1;
+            }
+        }
+    }
+    arsort($wordCountArr);
+    $wordCountArr = array_slice($wordCountArr, 0, 10);
+    return $wordCountArr;
+}
+
 $page_access_token = 'EAADIQGZBO8vQBAN86wOjzGZBFSFw2W8BqQZCBjrS9XwQGKnZCmaeWg17X8ZBXh2R9Arwgb9UkDOPXXUq0dVR8rLJpb3ojKUvOTG5ZBdR52etFT0qpn5rmLZBLuW5ZA5w5ESuce5vIpLxZAsSjoRaBVdov6R1Y5L8fxK4ZD';
 
 $page_id = '596434263827904';
@@ -24,6 +58,14 @@ $posts = mysqli_query($dbHandle, "
                       and a.company_id = b.id and a.gen_img_id != 0 limit 0,1");
 
 $post = mysqli_fetch_array($posts);
+$keywords = extractCommonWords($post['description']);
+//var_dump($keywords);
+foreach($keywords as $word => $t){
+    //echo $word."\n";
+    $post['description'] = str_ireplace($word,"#".$word,$post['description']);
+}
+$post['description'] = "#".str_replace(' ', '',ucwords($post['title'])) . ": " .$post['description'];
+
 //var_dump($post);
 if($post['link']){
     $data['picture'] = "http://api.file-dog.shatkonlabs.com/files/rahul/".$post['gen_img_id'];
