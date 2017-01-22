00d77df03fa6686acb881b4d8ef129f6c8655949
rahullahoria <rahul_lahoria@yahoo.com>
2017-01-22 16:55:12 +0530

    search, feedback request, invoice apis added

--- scripts/RagnarPostCreater.php
+++ scripts/RagnarPostCreater.php
@@ -16,6 +16,7 @@ $posts = mysqli_query($dbHandle, "
                   WHERE a.gen_img_id = 0 and a.`status` = 'not-approved' and a.company_id = b.id");
 
 while ( $post = mysqli_fetch_array($posts)) {
+    var_dump($post);
     $genId = getGenPost($post['raw_img_id'],$post['logo_id'],$post['description'],"Get ".$post['title']." Services<br>Get More Info <br>" ,$post['link']);
     if(isset($genId) && $genId != 0){
         $sql="UPDATE `ragnar_social`.`posts` SET `gen_img_id` = '".$genId."' WHERE `posts`.`id` =".$post['id'].";";
