85fe30f2d75057e2a46ff7d65dc7ad61911a38f4
anil-ninja <kumar.anil8892@gmail.com>
2017-01-23 11:29:59 +0530

    call logs

--- api/header.php
+++ api/header.php
@@ -25,14 +25,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
     exit(0);
 }
-if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
 
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: PUT, GET, POST, OPTIONS");
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-    exit(0);
-}
 ?>
